fc106f2a3d5c2a2aa459ec33ca5e65776021f7b7
Haider Ali <haiderali.shaikh@neosofttech.com>
2022-01-12 17:00:19 +0530

    Added Reset Counter

--- src/App.js
+++ src/App.js
@@ -24,20 +24,23 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     handleDecrementClick: () => dispatch({ type: 'DECREMENT' }),
-    handleIncrementClick: () => dispatch({ type: 'INCREMENT' })
+    handleIncrementClick: () => dispatch({ type: 'INCREMENT' }),
+    handleResetClick: () => dispatch({ type: 'RESET' }),
   }
 }
 
-function App({ count, handleDecrementClick, handleIncrementClick }) {
+function App({ count, handleDecrementClick, handleIncrementClick, handleResetClick }) {
   return (
     // <div className="app-container">
     //   <button className='btn btn-primary'>Add Counter</button>
     //   <p className='app-text'>Hello Redux! Here is your Counter { count}</p>
     // </div>
     <div className='container'>
-      <button type="button" className="btn btn-primary button" onClick={handleDecrementClick}>Decrement</button>
-      <button type="button" className="btn btn-secondary button" onClick={handleIncrementClick}>Increment</button>
-      <h1>Helloworld React & Redux!! {count}</h1>
+      <button type="button" className="btn btn-secondary button" onClick={handleDecrementClick}>Decrement</button>
+      <button type="button" className="btn btn-primary button" onClick={handleIncrementClick}>Increment</button>
+      <button type="button" className="btn btn-danger" onClick={handleResetClick}>Reset</button>
+      <h1>Helloworld React & Redux! Counter is </h1>
+      <p><span className='counter-text'>{count}</span></p>
     </div>
   );
 }
--- src/config/Reducer.js
+++ src/config/Reducer.js
@@ -4,6 +4,8 @@ const countReducer = function (state = 0, action) {
             return state + 1;
         case "DECREMENT":
             return state - 1;
+        case "RESET":
+            return 0;
         default:
             return state;
     }
